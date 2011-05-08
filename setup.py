2e45b88c31b11eb91e2f62eedd2785709ac848ac
Danilo Bargen <gezuru@gmail.com>
2011-05-08 14:25:16 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='upscrot',
+      version='0.1',
+      author='Danilo Bargen',
+      author_email='gezuru@gmail.com',
+      url='https://github.com/gwrtheyrn/upscrot/',
+      description='A very simple script that will take a screenshot using the linux "scrot" command and upload it directly to a SFTP server.',
+      platforms=['Unix'],
+      license='GPLv3',
+      requires=['gtk', 'pynotify'], 
+      py_modules=['upscrot'],
+      scripts=['upscrot.py'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Natural Language :: English',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2',
+          'Topic :: Desktop Environment :: Gnome',
+          'Topic :: Home Automation',
+          'Topic :: Utilities',
+          ],
+     )
--- upscrot.py
+++ upscrot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 # Directly upload screenshot to sftp server
 # Author: Danilo Bargen
