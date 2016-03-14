c302388c091b6b2e69a0e3a498626872b89e3bc5
Danilo Bargen <mail@dbrgn.ch>
2016-03-14 11:08:51 +0100

    Config file reading

--- upscrot.py
+++ upscrot.py
@@ -4,35 +4,88 @@
 # Author: Danilo Bargen
 # License: GPLv3
 
+import collections
+import configparser
+import datetime
+import os
 import subprocess
-from datetime import datetime
+import sys
 
-SERVER = 'example.org'
-TARGETDIR = '/var/www/tmp/screenshots/'
-URLROOT = 'http://%s/tmp/screenshots/' % SERVER
+import appdirs
 
-TIME = datetime.today().strftime('%Y%m%d_%H%M%S')
-FILE = '%s.png' % TIME
 
-try:
-    subprocess.check_call(['scrot', '-s', '/tmp/%s' % FILE])
-except subprocess.CalledProcessError as e:
-    print('Could not take screenshot: %s' % e)
-    exit(-1)
+def init_config():
+    """
+    Initialize config.
 
-try:
-    subprocess.check_call(['scp', '/tmp/%s' % FILE, '%s:%s' % (SERVER, TARGETDIR)])
-except subprocess.CalledProcessError as e:
-    print('Could not copy file to server: %s' % e)
-    exit(-1)
+    If config file exists, read it.
+    If not, initialize it.
 
-# X clipboard
-try:
-    clipboards = ['-pi', '-bi']
-    for clipboard in clipboards:
-        xsel = subprocess.Popen(['xsel', clipboard], stdin=subprocess.PIPE)
-        xsel.communicate(input=URLROOT + FILE)
-except OSError:
-    pass
+    """
+    confdir = appdirs.user_config_dir('upscrot')
+    try:
+        os.makedirs(confdir)
+    except FileExistsError:
+        pass
+    confpath = os.path.join(confdir, 'config.ini')
 
-print('%s%s' % (URLROOT, FILE))
+    # Initialize configparser
+    config = configparser.ConfigParser(dict_type=collections.OrderedDict)
+
+    # Read config file if it exists
+    if os.path.exists(confpath):
+        config.read(confpath)
+        return config
+
+    # Create it otherwise.
+    else:
+        config['upload'] = {
+            'target_host': 'example.org',
+            'target_dir': '/var/www/tmp/screenshots',
+            'base_url': 'https://example.org/tmp/screenshots/',
+        }
+        with open(confpath, 'w+') as f:
+            config.write(f)
+        print('Created initial config file.')
+        print('Please edit \'%s\' and then run upscrot again.' % confpath)
+        sys.exit(1)
+
+
+def main(config):
+    timestamp = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
+    filename = '%s.png' % timestamp
+
+    # Take screenshot
+    try:
+        subprocess.check_call(['scrot', '-s', '/tmp/%s' % filename])
+    except subprocess.CalledProcessError as e:
+        print('Could not take screenshot: %s' % e)
+        exit(-1)
+
+    # Upload file
+    try:
+        subprocess.check_call([
+            'scp',
+            '/tmp/%s' % filename,
+            '%s:%s' % (config['upload']['target_host'], config['upload']['target_dir']),
+        ])
+    except subprocess.CalledProcessError as e:
+        print('Could not copy file to server: %s' % e)
+        exit(-1)
+    url = config['upload']['base_url'] + filename
+
+    # X clipboard
+    try:
+        clipboards = ['-pi', '-bi']
+        for clipboard in clipboards:
+            xsel = subprocess.Popen(['xsel', clipboard], stdin=subprocess.PIPE)
+            xsel.communicate(input=url.encode('utf8'))
+    except OSError:
+        pass
+
+    print(url)
+
+
+if __name__ == '__main__':
+    config = init_config()
+    main(config)
