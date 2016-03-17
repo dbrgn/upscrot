f8b93f0b416dd0d9c8484573161cc884e324df3d
Danilo Bargen <mail@dbrgn.ch>
2016-03-17 22:57:48 +0100

    Release v1.0.0b2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
 
 setup(
     name='upscrot',
-    version='1.0.0b1',
+    version='1.0.0b2',
 
     description='Directly upload screenshots to a SSH server.',
     long_description=long_description,
